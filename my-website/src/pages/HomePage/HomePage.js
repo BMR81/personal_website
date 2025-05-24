f3e13d79cf170e02cf53ab9043a45ef04ca964aa
BMR81 <BMR81@pitt.edu>
2025-05-24 01:49:22 -0400

    Home Page is complete

--- my-website/src/pages/HomePage/HomePage.js
+++ my-website/src/pages/HomePage/HomePage.js
@@ -1,3 +1,13 @@
+import './HomePage.css';
+
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+        <div className="home-body">
+            <div id="intro-container">
+                <h1 className="fade-in" id="intro-text">Welcome to my personal website! Here, you can learn more about
+                    me.
+                </h1>
+            </div>
+        </div>
+    );
 }
